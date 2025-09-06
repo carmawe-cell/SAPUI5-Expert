b0e25455d16ae860c96c8c41ef3b4cb2e8960cfe
carmawe-cell <carmawe@gmial.com>
2025-09-06 04:34:51 +0000

    05 Estructura Proyectos SAPUI5

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,21 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+    //"sap/m/MessageToast"
+],
+    /**
+     * 
+     * @param {typeof sap.ui.core.mvc.Controller} Controller
+     * @param {typeof sap.m.MessageToast} MessageToast
+     * @param {typeof sap.ui.model.resource.ResourceModel} ResourceModel
+     */
+    function (Controller, MessageToast) {
+        "use strict";
+
+        return Controller.extend("logaligroup.sapui5.controller.App", {
+
+            onInit: function () {
+
+            }
+        });
+
+    });
